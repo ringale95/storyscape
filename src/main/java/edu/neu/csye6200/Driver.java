82bb9e2fe05c40141639f8a4ed6d19f24c153c37
Raveena Ingale <ingale.r@northeastern.edu>
2025-11-21 00:44:49 -0500

    user-entity-created-with-seed-data-for-user

--- src/main/java/edu/neu/csye6200/Driver.java
+++ src/main/java/edu/neu/csye6200/Driver.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Driver {
+
 	public static void main(String[] args) {
-		System.out.println("============Main Execution Start===================\n\n");
-
 		SpringApplication.run(Driver.class, args);
-
-
-		System.out.println("\n\n============Main Execution End===================");
 	}
 
 }
